ddca753c7c8bf72ee7f611b96c6d4d161fb87376
Joseba Portas Abalde <sepeto2000@gmail.com>
2021-11-21 21:43:25 +0100

    cripto-analysis 1.0

---
+++ src/components/PxButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- @click="buttonClick" -->
+  <button
+    class="
+      bg-transparent
+      hover:bg-green-500
+      text-green-700
+      font-semibold
+      hover:text-white
+    "
+  >
+    <slot> </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+
+  /*methods: {
+    buttonClick() {
+      this.$emit('custom-click')
+    }
+  }*/
+}
+</script>
